82e0106538e581e05247fbe4a647e145bfbe36df
DESKTOP\adona65 <adona65>
2022-06-08 15:27:55 +0200

    - Add informations about Records used with sealed keyword - Add informations about various changes in JVM

--- src/tutorial_000/languageNewFeatures/_003_Records.java
+++ src/tutorial_000/languageNewFeatures/_003_Records.java
@@ -13,12 +13,24 @@ public class _003_Records {
 			record RecordWithParameters (String name, String topic) {}
 		 *
 		 * And for instantiate it, we use classical java, as for any other objects :
+		 * 
 		   // Commented in order to avoid compilation error if preview features are not activated.
 		   RecordWithParameters myRecord = new RecordWithParameters("Toto", "This is a record");
 		 * 
 		 * Records are still a preview feature in Java 15, but some additions are done :
 		 * - native methods cannot be declared in records.
 		 * - implicit fields of record are not final and modification using reflection will throw IllegalAccessException.
+		 * 
+		 * Records may also works with the "sealed" preview feature. As records are final by default and can extend interfaces, we are
+		 * allowed to define sealed interfaces then let record implement them :
+		 * 
+		 	// Commented in order to avoid compilation error if preview features are not activated.
+		 	sealed interface MyInterface permits RecordWithParameters {
+			   int id();
+			   String name();
+			}
+			
+			record RecordWithParameters(int id, String name) implements MyInterface {}
 		 */
 	}
 }
---
+++ src/tutorial_000/languageNewFeatures/_004_JvmChanges.java
@@ -0,0 +1,48 @@
+package tutorial_000.languageNewFeatures;
+
+public class _004_JvmChanges {
+	/*
+	 * Java 15 delivered various changes concerning JVM. Here are some.
+	 */
+	
+	/*
+	 * Hidden Classes
+	 * 
+	 * Java 15 introduce hidden classes which cannot be used directly by other classes bytecode. These hidden classes are intended to be 
+	 * used by frameworks which generate classes at runtime and use them using reflection. 
+	 * 
+	 * A hidden class is defined as a member of Nest Based Access Control Context and it can be unloaded irrespective of other classes.
+	 */
+	
+	/*
+	 * Garbage Collectors
+	 * 
+	 * Since Java 11, Z Garbage Collector (ZGC) was an experimental feature. Java 15 release ZGC and made it a standard feature.
+	 * ZGC is low latency, highly scalable garbage collector. A lot of improvements are done to this garbage collection since then, for example :
+	 * - Concurrent class unloading
+	 * - Uncommiting of unused memory
+	 * - Support for Class Data Sharing
+	 * - NUMA Awareness
+	 * - Multithreaded Heap Pre-touch
+	 * - Max Heap Size limit from 4 TB to 16 TB.
+	 * 
+	 * ZGC is highly performant and works efficiently even in case of massive data applications e.g. machine learning applications. It ensures 
+	 * that there is no long pause while processing the data due to garbage collection. It supports Linux, Windows and MacOS.
+	 */
+	
+	/*
+	 * Helpful NullPointerException
+	 * 
+	 * Helpful NullPointerException is now enabled by default in Java 15.
+	 */
+	
+	/*
+	 * Foreign Memory Access API
+	 * 
+	 * Java 15 add some features to Foreign Memory Access API that was introduced in Java 14 :
+	 * - VarHandle API introduced to customize memory access var handles.
+	 * - Parallel processing of a memory segment is supported using the Spliterator interface.
+	 * - Parallel processing of a memory segment is supported using the Spliterator interface.
+	 * - Native calls addresses can be manipulated and dereferenced.
+	 */
+}
